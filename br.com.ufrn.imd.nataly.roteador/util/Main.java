76db0b3f95a703e184fc08ce952e103f25e228d5
Vanz5 <36575665+Vanz5@users.noreply.github.com>
2019-10-26 19:08:22 -0300

    Inicializando portas e sem NullPointerException

--- br.com.ufrn.imd.nataly.roteador/util/Main.java
+++ br.com.ufrn.imd.nataly.roteador/util/Main.java
@@ -1,5 +1,6 @@
 package util;
 
+import conections.Porta;
 import routers.Roteador;
 
 public class Main {
@@ -7,33 +8,37 @@ public class Main {
     public static void main(String[] args){
 
         //Criando roteadores em uma matriz 3x3 e setando as portas
-        Roteador[][] r = new Roteador[3][3];
+        Roteador[][] roteadores = new Roteador[3][3];
 
         //adicionando os ips dados e definindo as portas conectadas a cada roteador :
         int aux = 0;
         int j = 0;
-        for (int i = 0; i < r.length; i++){;
-            for(j = 0; j < r[i].length; j++){
-                r[i][j] = new Roteador();
-                r[i][j].setIP(new StringBuilder().append("192.168.0.").append(aux).toString());
+        for (int i = 0; i < roteadores.length; i++){;
+            for(j = 0; j < roteadores[i].length; j++){
+                roteadores[i][j] = new Roteador(); //inicializando os roteadores
+                roteadores[i][j].setIP(new StringBuilder().append("192.168.0.").append(aux).toString());
                 aux++;
-                r[i][j].setPosJ(j);
-                r[i][j].setPosI(i);
+                roteadores[i][j].setPosJ(j);
+                roteadores[i][j].setPosI(i);
+                roteadores[i][j].PCima = new Porta(); //inicializando as portas
+                roteadores[i][j].PBaixo = new Porta();;
+                roteadores[i][j].PEsquerda = new Porta();
+                roteadores[i][j].PDireita = new Porta();
                 //conections.Porta Cima:
                 if(i != 0) {
-                    r[i][j].PCima.setRoteadorDestino(r[i-1][j]);
+                    roteadores[i][j].PCima.setRoteadorDestino(roteadores[i-1][j]);
                 }
                 //conections.Porta Baixo:
                 if(i != 2){
-                    r[i][j].PBaixo.setRoteadorDestino(r[i+1][j]); //Apesar do NullPointerException ta indicando aqui, ele tbm acontece nos outros set L24, L32 e L36
+                    roteadores[i][j].PBaixo.setRoteadorDestino(roteadores[i+1][j]);
                 }
                 //conections.Porta Esquerda
                 if(j != 0){
-                    r[i][j].PEsquerda.setRoteadorDestino(r[i][j-1]);
+                    roteadores[i][j].PEsquerda.setRoteadorDestino(roteadores[i][j-1]);
                 }
                 //conections.Porta Direta
                 if(j != 2){
-                    r[i][j].PDireita.setRoteadorDestino(r[i][j+1]);
+                    roteadores[i][j].PDireita.setRoteadorDestino(roteadores[i][j+1]);
                 }
             }
         }
